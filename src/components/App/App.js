fab374f553d956ff91ae58180b9c75ed41bf9175
phuocdv <dovanphuocht@gmail.com>
2021-10-10 08:50:07 +0700

    them basename o router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,42 +1,38 @@
-import React from 'react';
+import React from 'react'
 import '../../assets/styles/global.scss'
 import { Grid, Row, Column } from '@mycv/mycv-grid'
 import config from '../../config'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
-import Header from '../../container/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from '../../container/Header'
 import Home from '../../container/Home'
-import FollowContainer from '../../container/FollowContainer';
+import FollowContainer from '../../container/FollowContainer'
 import Sidebar from '../../container/Sidebar'
 import PostDetailModal from '../../container/Home'
 import Profile from '../../container/Profile'
 
 const App = () => {
-    return (
-        <Router>
-            <Grid>
-                <Header />
-                <Grid type="wide" maxWidth={config.mainWidth}>
-                    <Row>
-                        <Column sizeDesktop={4}>
-                            <Sidebar />
-                        </Column>
-                        <Column sizeDesktop={8}>
-                            <Switch>
-                                <Route exact path={config.routes.home} component={Home}></Route>
-                                <Route path={config.routes.follow} component={FollowContainer}></Route>
-                                <Route path={config.routes.postDetail} component={PostDetailModal}></Route>
-                                <Route path={config.routes.profile} component={Profile}></Route>
-                            </Switch>
-                        </Column>
-                    </Row>
-                </Grid>
-            </Grid>
-        </Router>
-    );
-};
+  return (
+    <Router basename="/reactjs-tiktok-app">
+      <Grid>
+        <Header />
+        <Grid type="wide" maxWidth={config.mainWidth}>
+          <Row>
+            <Column sizeDesktop={4}>
+              <Sidebar />
+            </Column>
+            <Column sizeDesktop={8}>
+              <Switch>
+                <Route exact path={config.routes.home} component={Home}></Route>
+                <Route path={config.routes.follow} component={FollowContainer}></Route>
+                <Route path={config.routes.postDetail} component={PostDetailModal}></Route>
+                <Route path={config.routes.profile} component={Profile}></Route>
+              </Switch>
+            </Column>
+          </Row>
+        </Grid>
+      </Grid>
+    </Router>
+  )
+}
 
-export default App;
+export default App
